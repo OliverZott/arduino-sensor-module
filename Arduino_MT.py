d705e9e38b8b79e288096c2b6debb473eb54f43a
Oliver Zott <oliver.zott@edu.umit.at>
2019-04-08 17:17:03 +0200

    doc.txt: extendet / Python Script: lux-sensor; loop for logging

--- Arduino_MT.py
+++ Arduino_MT.py
@@ -8,62 +8,92 @@
 import serial
 import re
 import datetime
+import time
+
+
+
 
 
 # ============= NEXT =============
-# Alles in Funktionen packen
-# OOP ???
-# Loop für minütliches Logging / oder Batchjob aufruf ?!
+# OOP ?
+# function arguments, parameters ok like that?
+# check clock-time for Serial-communication (15 sec ???) and adjust loop-timing
+
+
 
 
 
 #===============================================================
 # Serial communication & decoding
 #===============================================================
-print("Serial Port - Test:")
-port = serial.Serial()
-port.baudrate = 9600
-port.port = 'COM4'
-port.open()
-port_out = port.read(61)
-#print (port);
-print ("Data: ", port_out);
-print ("Type: ", type(port_out));
 
-# Decode byte stream (byte --> str)
-data_str = port_out.decode("utf-8") 
-#print ("Data: ", data_str)
-#print ("Type: ", type(data_str))
-
+def serial_open():
+	port = serial.Serial()
+	port.baudrate = 9600
+	port.port = 'COM4'
+	port.open()
+	port_out = port.read(75)  #(72 min)
+	#print (port);
+	#print ("Data: ", port_out);
+	#print ("Type: ", type(port_out));
+	
+	# Decode byte stream (byte --> str)
+	data_str = port_out.decode("utf-8")  	
+	return data_str
+	
+	#print ("Data: ", data_str)
+	#print ("Type: ", type(data_str))
 
 
 #===============================================================
 # RegEx
 #===============================================================
 
-temp_re = re.search(r"\d*\s\*C", data_str).group(0)		# group(0), damit str (<class 're.Match'>)
-#print ("temp_re", temp_re)
-#print ("Typ von temp_re", type(temp_re))
-temp = temp_re.rstrip(" *C")
-#print (temp)
-#print ("Typ von temp", type(temp))
+def values_re(data_str):
+	temp_re = re.search(r"\d*\s\*C", data_str).group(0)		# group(0), damit str (<class 're.Match'>)
+	#print ("temp_re", temp_re)
+	#print ("Typ von temp_re", type(temp_re))
+	temp = temp_re.rstrip(" *C")
+	#print (temp)
+	#print ("Typ von temp", type(temp))
 
-hum_re = re.search(r"\d*\s\%", data_str).group(0)
-hum = hum_re.rstrip(" %")
+	hum_re = re.search(r"\d*\s\%", data_str).group(0)
+	hum = hum_re.rstrip(" %")
 
+	lux_re = re.search(r"\d*\.\d*\slux", data_str).group(0)
+	lux = lux_re.rstrip(" lux")
+	
+	return temp, hum, lux
 
 
 #===============================================================
 # File IO
 #===============================================================
 
-date = datetime.datetime.now().strftime("%Y-%m-%d")								# date for filename
-datetime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")				# date & time
-measurement = datetime + ":" " Temperature: " + temp + " C°, " + "Humidity: " + hum + " %\n"	# meassurement-String
+def file_io (temp, hum, lux):
+	date = datetime.datetime.now().strftime("%Y-%m-%d")							# date for filename
+	dateANDtime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")			# date & time
+	
+	# meassurement-String
+	measurement = dateANDtime + ":" " Temperature: " + temp + " C°, " + "Humidity: " + hum + " %, " + "Illuminance: " + lux + " lux" + "\n"	
+	
+	file_name = "Messwerte_"  + date +".txt"; 
+	file = open(file_name, "a")
+	file.write(measurement)
+	file.close()
+	
 
-file_name = "Messwerte_"  + date +".txt"; 
-file = open(file_name, "a")
-file.write(measurement)
-file.close()
+#===============================================================
+# Loop
+#===============================================================
 
+print("Loop for data-logging starts...")
 
+while True:
+	t_s = datetime.datetime.now().second
+	t_h = t = datetime.datetime.now().hour
+	if 15 <= t_h < 17 and t_s == 00:
+		serial_open()	
+		values_re(serial_open())
+		file_io(*values_re(serial_open()))
+		#time.sleep(5)
